d3f9c5b7c6e2a48c902a848991ed672d7e1ca49e
Jackie <brundtoe@outlook.dk>
2021-07-08 10:27:15 +0200

    oprettet utmabs med main function

---
+++ go.mod
@@ -0,0 +1,7 @@
+module example.com/utmabs
+
+go 1.16
+
+replace example.com/convert => ../convert
+
+require example.com/convert v0.0.0-00010101000000-000000000000
---
+++ utmabs.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"example.com/convert"
+	"fmt"
+	"log"
+	"strings"
+)
+
+/*
+Som rust udgaven hånderes kmkv angive med 1 m nøjagtighed dvs 5 cifre
+*/
+
+func main() {
+	fmt.Println("Hello Jackie")
+
+	//koord := Mgrs{"32", "U", "NG", "08600", "77000", "Somewhere"}
+
+	koordCsv := "32;U;NG;08600;77000;Somewhere"
+
+	ka := strings.Split(koordCsv, ";")
+
+	koord := convert.Mgrs{ka[0], ka[1], ka[2], ka[3], ka[4], ka[5]}
+
+	result, err := convert.UtmAbs(koord)
+
+	log.SetPrefix("Error: ")
+	log.SetFlags(0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("zone %d\t", result.Zone)
+	fmt.Printf("belt %s\t", result.Belt)
+	fmt.Printf("east %s\t", koord.East)
+	fmt.Printf("north %s\t", koord.North)
+	fmt.Printf("easting %.0f\t", result.Easting)
+	fmt.Printf("northing %.0f\n", result.Northing)
+
+}
