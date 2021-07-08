ec92629d5e99a54be62406bac91dc076ef6f7218
Jackie <brundtoe@outlook.dk>
2021-07-08 16:48:21 +0200

    tilføjet indlæsning af cities.csv

---
+++ cmd/cities/cities.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("/home/jackie/dumps/geografi/cities.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
