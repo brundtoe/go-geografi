bbd8222b012d310a1c98cc01f12b5ef5d2bb514f
Jackie <brundtoe@outlook.dk>
2021-07-10 18:33:48 +0200

    omdøbt convert.go til utmabs.go

--- convert/utmabs_test.go
+++ convert/utmabs_test.go
@@ -32,24 +32,30 @@ func TestZoneTable(t *testing.T) {
 
 	var tests = []struct {
 		input string
+		field string
 		want  string
 	}{
-		{"32;U;NG;08600;77000;Somewhere", "pass"},
-		{"3x2;U;NG;08600;77000;Somewhere", "fail"},
-		{"321;U;NG;08600;77000;Somewhere", "fail"},
-		{"-11;U;NG;08600;77000;Somewhere", "fail"},
+		{"32;U;NG;08600;77000;Somewhere", "zone", "pass"},
+		{"3x2;U;NG;08600;77000;Somewhere", "zone", "fail"},
+		{"321;U;NG;08600;77000;Somewhere", "zone", "fail"},
+		{"-11;U;NG;08600;77000;Somewhere", "zone", "fail"},
+		{"32;N;NG;08600;77000;Somewhere", "belt", "pass"},
+		{"32;X;NG;08600;77000;Somewhere", "belt", "pass"},
+		{"32;U;G;08600;77000;Somewhere", "kmkv", "fail"},
+		{"32;U;G;08600;77000;Somewhere", "kmkv", "fail"},
+		{"32;U;NV;08600;77000;Somewhere", "kmkv", "pass"},
+		{"32;U;NW;08600;77000;Somewhere", "kmkv", "fail"},
 	}
 
 	for _, test := range tests {
 		koord := buildKoord(test.input)
 		_, err := UtmAbs(koord)
 		if test.want == "pass" && err != nil {
-			t.Errorf("Zone = %s cannot be converted to int", koord.Zone)
+			t.Errorf("Field %s : %s", test.field, err)
 		}
 		if test.want == "fail" && err == nil {
-			t.Errorf("test fails Zone = %s er ikke en valid zone", koord.Zone)
+			t.Errorf("Field %s : %s", test.field, err)
 		}
 
 	}
-
 }
