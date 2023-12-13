ec44e1a9ceaab0cb29612656eaf8abed3a8d450b
Jackie <brundtoe@outlook.dk>
2023-12-13 17:35:57 +0100

    cmd utmgeo med taylor algoritmen fjernet coco konverteringer

--- cmd/utmgeo/utmgeo.go
+++ cmd/utmgeo/utmgeo.go
@@ -3,10 +3,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/brundtoe/go-geografi/pkg/coco"
 	"github.com/brundtoe/go-geografi/pkg/taylor"
 	"math"
-	"strconv"
 )
 
 func main() {
@@ -20,16 +18,6 @@ func main() {
 	fmt.Printf("East\t %4f\n", east)
 	fmt.Printf("North\t %4f\n", north)
 
-	location := coco.LL{
-		Lat: latitude,
-		Lon: longitude,
-	}
-	utm := location.ToUTM()
-
-	fmt.Println("UTM..", utm)
-	fmt.Println("MGRS..", utm.ToMGRS(1))
-	fmt.Println("USNG..", utm.ToUSNG(1))
-
 	lat, lon := taylor.UTMXYToLatLon(east, north, 32, false)
 
 	fmt.Printf("Lat\t %4f\n", taylor.RadToDeg(lat))
@@ -42,15 +30,4 @@ func main() {
 		fmt.Printf("Konvertering af longitude overskrider acceptabel tolerance")
 	}
 
-	milgrid := coco.MGRS("32VNJ9485799059")
-
-	utmsted, _, _ := milgrid.ToUTM()
-	println("UTM", utmsted.String())
-	mgrs := coco.USNG("32V NJ 94857 99059").ToMGRS()
-	usngUtm, _, _ := coco.USNG("32V NJ 94857 99059").ToUTM()
-	println("USNG UTM..", usngUtm.String())
-	llSted, accuracy, _ := mgrs.ToLL()
-	latt := fmt.Sprintf("%f", llSted.Lat)
-	long := fmt.Sprintf("%f", llSted.Lon)
-	println(latt, long, strconv.Itoa(accuracy))
 }
