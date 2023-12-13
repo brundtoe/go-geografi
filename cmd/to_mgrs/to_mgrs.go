0f20d4af65b87815e9302bc3e3dc3ffe89d70a69
Jackie <brundtoe@outlook.dk>
2023-12-13 17:24:31 +0100

    ny command to_mgrs.go som anvender package coco

---
+++ cmd/to_mgrs/to_mgrs.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/brundtoe/go-geografi/pkg/coco"
+	"github.com/brundtoe/go-geografi/pkg/utils"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+type City struct {
+	name         string
+	zip          string
+	municipality string
+	region       string
+	population   int64
+	latitude     float64
+	longitude    float64
+	zone         int64
+	belt         string
+	kmkv         string
+	east         int64
+	north        int64
+	easting      float64
+	northing     float64
+}
+
+func main() {
+
+	part := "geografi/cities.csv"
+	filename, err := utils.GetDataPath(part, "PLATFORM")
+	if err != nil {
+		fmt.Printf("Det er ikke muligt at finde path filen %s: %s", part, err)
+		os.Exit(1)
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r := csv.NewReader(file)
+	r.Comma = ';'
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		var city City
+		// first line contains field names
+		if record[1] != "City" {
+			city.buildCity(record)
+			milgrid := city.toMgrs()
+			usng := city.toUsng()
+			fmt.Printf("%-18s %s %s\n", city.name, milgrid, usng)
+		}
+	}
+}
+
+func (city City) toMgrs() coco.MGRS {
+	location := coco.LL{
+		Lat: city.latitude,
+		Lon: city.longitude,
+	}
+	mgrs, _ := location.ToMGRS(1)
+	return mgrs
+
+}
+
+func (city City) toUsng() coco.USNG {
+	location := coco.LL{
+		Lat: city.latitude,
+		Lon: city.longitude,
+	}
+	utm := location.ToUTM()
+	usng := utm.ToUSNG(1)
+	return usng
+
+}
+
+func (city *City) buildCity(koord []string) {
+	city.name = koord[1]
+	city.zip = koord[2]
+	city.municipality = koord[3]
+	city.region = koord[4]
+	city.population, _ = strconv.ParseInt(koord[5], 10, 64)
+	city.latitude, _ = strconv.ParseFloat(koord[6], 64)
+	city.longitude, _ = strconv.ParseFloat(koord[7], 64)
+	city.zone, _ = strconv.ParseInt(koord[8], 10, 64)
+	city.belt = koord[9]
+	city.kmkv = koord[10]
+	city.east, _ = strconv.ParseInt(koord[11], 10, 64)
+	city.north, _ = strconv.ParseInt(koord[12], 10, 64)
+	city.easting, _ = strconv.ParseFloat(koord[13], 64)
+	city.northing, _ = strconv.ParseFloat(koord[14], 64)
+
+}
