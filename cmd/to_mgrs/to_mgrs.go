487585358a1df413efefd690ee1f5a59fba9cc03
Jackie <brundtoe@outlook.dk>
2023-12-14 11:52:26 +0100

    refaktoreret ekstraheret struct City opg methods til ny geoutm modul

--- cmd/to_mgrs/to_mgrs.go
+++ cmd/to_mgrs/to_mgrs.go
@@ -8,24 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
-	"strconv"
 )
-
-type City struct {
-	name         string
-	zip          string
-	municipality string
-	region       string
-	population   int64
-	geoloc       geoutm.LL
-	zone         int64
-	belt         string
-	kmkv         string
-	east         int64
-	north        int64
-	easting      float64
-	northing     float64
-}
 
 func main() {
 
@@ -56,52 +39,13 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		var city City
+		location := geoutm.City{}
 		// first line contains field names
 		if record[1] != "City" {
-			city.buildCity(record)
-			milgrid := city.toMgrs()
-			usng := city.toUsng()
-			fmt.Printf("%-18s %s %s\n", city.name, milgrid, usng)
+			location.BuildCity(record)
+			milgrid := location.CityToMgrs()
+			usng := location.CityToUsng()
+			fmt.Printf("%-18s %s %s\n", location.Name, milgrid, usng)
 		}
 	}
-}
-
-func (city City) toMgrs() geoutm.MGRS {
-	location := geoutm.LL{
-		Lat: city.geoloc.Lat,
-		Lon: city.geoloc.Lon,
-	}
-	mgrs, _ := location.ToMGRS(1)
-	return mgrs
-
-}
-
-func (city City) toUsng() geoutm.USNG {
-	location := geoutm.LL{
-		Lat: city.geoloc.Lat,
-		Lon: city.geoloc.Lon,
-	}
-	utm := location.ToUTM()
-	usng := utm.ToUSNG(1)
-	return usng
-
-}
-
-func (city *City) buildCity(koord []string) {
-	city.name = koord[1]
-	city.zip = koord[2]
-	city.municipality = koord[3]
-	city.region = koord[4]
-	city.population, _ = strconv.ParseInt(koord[5], 10, 64)
-	city.geoloc.Lat, _ = strconv.ParseFloat(koord[6], 64)
-	city.geoloc.Lon, _ = strconv.ParseFloat(koord[7], 64)
-	city.zone, _ = strconv.ParseInt(koord[8], 10, 64)
-	city.belt = koord[9]
-	city.kmkv = koord[10]
-	city.east, _ = strconv.ParseInt(koord[11], 10, 64)
-	city.north, _ = strconv.ParseInt(koord[12], 10, 64)
-	city.easting, _ = strconv.ParseFloat(koord[13], 64)
-	city.northing, _ = strconv.ParseFloat(koord[14], 64)
-
 }
---
+++ pkg/geoutm/city.go
@@ -0,0 +1,58 @@
+package geoutm
+
+import "strconv"
+
+type City struct {
+	Name         string
+	Zip          string
+	Municipality string
+	Region       string
+	Population   int64
+	Geoloc       LL
+	Utm          UTM
+	Zone         int64
+	Easting      float64
+	Northing     float64
+	Belt         string
+	Kmkv         string
+	East         int64
+	North        int64
+}
+
+func (city *City) CityToMgrs() MGRS {
+	location := city.Geoloc
+	mgrs, _ := location.ToMGRS(1)
+	return mgrs
+
+}
+
+func (city *City) CityToUsng() USNG {
+	location := LL{
+		Lat: city.Geoloc.Lat,
+		Lon: city.Geoloc.Lon,
+	}
+	utm := location.ToUTM()
+	usng := utm.ToUSNG(1)
+	return usng
+
+}
+
+func (city *City) BuildCity(koord []string) {
+	city.Name = koord[1]
+	city.Zip = koord[2]
+	city.Municipality = koord[3]
+	city.Region = koord[4]
+	city.Population, _ = strconv.ParseInt(koord[5], 10, 64)
+	city.Geoloc.Lat, _ = strconv.ParseFloat(koord[6], 64)
+	city.Geoloc.Lon, _ = strconv.ParseFloat(koord[7], 64)
+	zoneNumber, _ := strconv.ParseInt(koord[8], 10, 64)
+	city.Utm.ZoneNumber = int(zoneNumber)
+	zoneLetter := []byte(koord[9])
+	city.Utm.ZoneLetter = zoneLetter[0]
+	city.Utm.Easting, _ = strconv.ParseFloat(koord[13], 64)
+	city.Utm.Northing, _ = strconv.ParseFloat(koord[14], 64)
+	city.Kmkv = koord[10]
+	city.East, _ = strconv.ParseInt(koord[11], 10, 64)
+	city.North, _ = strconv.ParseInt(koord[12], 10, 64)
+
+}
