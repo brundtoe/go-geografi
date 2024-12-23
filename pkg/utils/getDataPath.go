3845cd1456e1931e741f3940f6f936cf4eb315bd
Jackie <brundtoe@outlook.dk>
2024-12-23 16:33:29 +0100

    host omdøbt fra komplett til victoria

--- pkg/utils/getDataPath.go
+++ pkg/utils/getDataPath.go
@@ -14,7 +14,7 @@ func GetDataPath(part string, envvar string) (string, error) {
 	}
 	if hostType == "kvm" {
 		return filepath.Join("/nfs/data", part), nil
-	} else if hostType == "komplett" {
+	} else if hostType == "victoria" {
 		return filepath.Join("/home/projects/devops/data", part), nil
 	} else if hostType == "" {
 		return "", errors.New("Envvar er fundet men der er ikke angivet en værdi")
